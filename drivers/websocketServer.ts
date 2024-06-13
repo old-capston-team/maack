532985af6b618c94e6914a01cdaee02914163757
wingyou <winwing.you@gmail.com>
2024-06-13 15:12:36 +0900

    feat: 웹소켓 테스트 드라이버 추가

---
+++ drivers/websocketServer.ts
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const expressWs = require("express-ws")(app);
+const port = 3000;
+
+app.ws("/echo", (ws, req) => {
+  ws.on("message", (msg) => {
+    ws.send(msg);
+  });
+});
+
+app.ws("/tracking_progress", (ws, req) => {
+  let initialized = false;
+  let index = 0;
+  ws.on("message", (msg) => {
+    console.log("got message: " + msg);
+    if (!initialized) {
+      initialized = true;
+      ws.send(JSON.stringify({ isStarted: true }));
+    } else {
+      ws.send(
+        JSON.stringify({
+          best_start: index,
+          best_end: index + 8,
+          isFinished: index > 200,
+        }),
+      );
+      index += 8;
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
